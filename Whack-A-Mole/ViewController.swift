da9caa1878ec15bbf6d58599a22d6e5cb57c1329
Ryan Wixon <ryanwixon02@gmail.com>
2020-10-15 11:56:13 -0500

    Added score board and field

--- Whack-A-Mole/ViewController.swift
+++ Whack-A-Mole/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var screenWidth = 0
+    var screenHeight = 0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let screenBounds: CGRect = UIScreen.main.bounds
+        screenWidth = Int(screenBounds.width)
+        screenHeight = Int(screenBounds.height)
+        
+        let scoreBox = UILabel(frame: CGRect(x:20, y:40, width: screenWidth / 5, height: screenHeight / 10))
+        scoreBox.textAlignment = NSTextAlignment.center
+        scoreBox.backgroundColor = UIColor.blue
+        scoreBox.textColor = UIColor.white
+        scoreBox.text = "0"
+        
+        let groundColor = UILabel(frame: CGRect(x:20, y:40, width: screenWidth - 40, height: screenHeight - 60))
+        groundColor.backgroundColor = UIColor.green
+        
+        view.addSubview(groundColor)
+        view.addSubview(scoreBox)
     }
 
-
+    
 }
 
