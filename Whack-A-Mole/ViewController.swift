68d9032d5141a8b4e6c5668280a3b7f2ca0c3f18
Ryan Wixon <ryanwixon02@gmail.com>
2020-10-15 11:14:37 -0500

    Initial Commit

---
+++ Whack-A-Mole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whack-A-Mole
+//
+//  Created by Wixon, Ryan P on 10/15/20.
+//  Copyright © 2020 Wixon, Ryan P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
